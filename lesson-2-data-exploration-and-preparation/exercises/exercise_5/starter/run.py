248f194ce98b338259474251b825fec9927dc08b
AndreaCaliandro <andrea.caliandro@gmail.com>
2023-09-20 21:23:44 -0700

    exercise_5

--- lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -1,50 +1,53 @@
 #!/usr/bin/env python
-import argparse
+
 import logging
 import pandas as pd
 import wandb
+import os
+import hydra
+from omegaconf import DictConfig
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
-def go(args):
+@hydra.main(config_name='config')
+def go(config: DictConfig):
 
-    run = wandb.init(project="exercise_5", job_type="process_data")
+    wandb.login(key=config['wandb']['api_key'])
+    run = wandb.init(project=config['wandb']['project'], job_type=config['wandb']['job_type'])
 
-    ## YOUR CODE HERE
-    pass
+    logger.info("Reading artifact")
+    df = pd.read_parquet(config['artifact']['path'])
+
+    # Drop the duplicates
+    logger.info("Dropping duplicates")
+    df = df.drop_duplicates().reset_index(drop=True)
+
+    logger.info("Fixing missing values")
+    # These are missing values that are due to an old version of the data. On new data,
+    # because of a change in the web form used to register new songs, the title and the
+    # song name are already empty strings
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
+
+    filename = "processed_data.csv"
+    df.to_csv(filename)
+
+    artifact = wandb.Artifact(
+        name=config['artifact']['name'],
+        type=config['artifact']['type'],
+        description=config['artifact']['description'],
+    )
+    artifact.add_file(filename)
+
+    logger.info("Logging artifact")
+    run.log_artifact(artifact)
+
+    os.remove(filename)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Preprocess a dataset",
-        fromfile_prefix_chars="@",
-    )
-
-    parser.add_argument(
-        "--input_artifact",
-        type=str,
-        help="Fully-qualified name for the input artifact",
-        required=True,
-    )
-
-    parser.add_argument(
-        "--artifact_name", type=str, help="Name for the artifact", required=True
-    )
-
-    parser.add_argument(
-        "--artifact_type", type=str, help="Type for the artifact", required=True
-    )
-
-    parser.add_argument(
-        "--artifact_description",
-        type=str,
-        help="Description for the artifact",
-        required=True,
-    )
-
-    args = parser.parse_args()
-
-    go(args)
+    go()
